c71e696a259dbe03b3a800b31f40b06ca8f67fc5
Willlumm <william.lumme@gmail.com>
2022-09-26 20:34:19 +0100

    Blank compass

--- transformer.py
+++ transformer.py
@@ -1,9 +1,25 @@
 import numpy as np
 from PIL import Image
 
-with Image.open(r"data\1662142130_5.png") as img:
-    img = img.crop((10, 8, 70, 68))
-    img = img.convert(mode="L")
-    img.show()
-    img_data = np.asarray(img)
-    print(img_data)
+def process_img(path):
+    with Image.open(path) as img:
+        img = img.crop((11, 8, 71, 68))
+        img = img.convert(mode="L")
+        img_data = np.asarray(img)
+        return img_data
+
+def display(img):
+    output = ""
+    for row in img:
+        for n in row:
+            output += str(n)
+        output += "\n"
+    print(output)
+    
+blank = process_img(r"blank_compass.png")
+compass = process_img(r"data\1662142130_5.png")
+diff = np.abs(compass - blank)
+diff[diff > 0] = 1
+display(diff)
+
+
